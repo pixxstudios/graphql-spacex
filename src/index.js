a8c9ae83db0a9b65c4f69a4273f19706d031baa4
Gagandeep Singh <gagan.ddun@gmail.com>
2019-11-20 23:02:04 +1100

    updated config

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class SpaceX extends React.Component {
+    render() {
+        return (<h3>hello</h3>);
+    }
+}
+
+ReactDOM.render(<SpaceX />, document.getElementById('root'));
