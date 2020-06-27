560b7d7d188454e244b0ebe4fed53c382ee825a9
Gagandeep Singh <gagan.ddun@gmail.com>
2020-06-27 22:46:31 +1000

    loader

---
+++ src/components/loader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './loader.css';
+
+const Loader = () => (<div className="loaderWrapper">
+<div className="lds-dual-ring"></div>
+</div>);
+
+export default Loader;
--- src/components/ships.js
+++ src/components/ships.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import SHIPS_QUERY from '../queries/index';
+import Loader from './loader';
 
 const Ships = () => {
     const { loading, error, data } = useQuery(SHIPS_QUERY);
 
-    if(loading) return <h3>loading</h3>;
+    if(loading) return <Loader />;
     if(error) return <h3>error</h3>;
-    console.log(data);
+
     return data.ships.map(ship => (
             <h4 key={ship.id}>{ship.name}</h4>
         )
