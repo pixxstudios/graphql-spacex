5001c4536adbefc62eb5c88fec513f71ac038f2a
Gagandeep Singh <gagan.ddun@gmail.com>
2020-06-28 18:57:04 +1000

    added react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Ships from './components/ships';
 
 const client = new ApolloClient({
-  uri: 'https://api.spacex.land/graphql/'
+  uri: 'https://api.spacex.land/graphql/',
 });
 
-
 const App = () => (
-  <ApolloProvider client={client}>
-    <Ships />
-  </ApolloProvider>
-)
+  <Router>
+    <ApolloProvider client={client}>
+      <Switch>
+      <Route path="/ship">
+          <h3>ship</h3>
+        </Route>
+        <Route path="/">
+          <Ships />
+        </Route>
+      </Switch>
+    </ApolloProvider>
+  </Router>
+);
 
 export default App;
---
+++ src/queries/ship.js
@@ -0,0 +1,33 @@
+import {
+    gql
+} from 'apollo-boost';
+
+const GET_SHIP_QUERY = gql `
+{
+    ship(id: "GOMSTREE") {
+      id
+      class
+      attempted_landings
+      active
+      abs
+      url
+      model
+      name
+      roles
+      speed_kn
+      status
+      type
+      successful_landings
+      missions {
+        flight
+        name
+      }
+      image
+      home_port
+      course_deg
+      weight_kg
+    }
+  }
+`;
+
+export default GET_SHIP_QUERY;
